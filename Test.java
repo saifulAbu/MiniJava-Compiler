016f9ba09adec9b6ab460d40cca644681598b9fe
siddartha saif <saifulabu25@gmail.com>
2017-02-04 22:09:51 -0600

    first commit

---
+++ Test.java
@@ -0,0 +1,31 @@
+/*
+created by Saiful Abu
+For learning more about compiler
+*/
+//this is part of bucket list project
+class AA {
+    public static void main(String [] args){
+        //variable declartions should be grouped toghether
+        int a;
+        int b;
+        int i;
+       
+        //statments should be grouped together
+        a = 3 + 4; 
+        i = 0;
+        while(i < 3){
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
+
+class BB {
+    public int sum(){
+        int a;
+        int b;
+        a = 3;
+        return 0;
+    } 
+}
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+
+public class Main {
+   
+   public static void main(String [] args) throws FileNotFoundException {
+      try {
+         System.setIn(new FileInputStream("Test.java"));
+         new MiniJavaParser(System.in).Program();
+         System.out.println("Lexical analysis successfull");
+      }
+      catch (ParseException e) {
+         System.out.println("Lexer Error : \n"+ e.toString());
+      }
+   }
+}
+
+
